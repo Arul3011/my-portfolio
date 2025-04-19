2e000da11edecccbcdcf761cf6f5c85fd0550ea2
arul <153939531+Arul3011@users.noreply.github.com>
2025-04-19 09:09:50 +0000

    change card height to auto

--- src/compountes/Card.jsx
+++ src/compountes/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({
     btnHref,
   }) => {
     return (
-      <div className="mb-10 overflow-hidden w-96 h-80 rounded-2xl mx-5 bg-neutral-900 shadow-lg duration-300 hover:shadow-xl border border-neutral-700" data-aos="flip-left">
+      <div className="mb-10 h-auto overflow-hidden w-96 h-80 rounded-2xl mx-5 bg-neutral-900 shadow-lg duration-300 hover:shadow-xl border border-neutral-700" data-aos="flip-left">
     
       <div className="p-6 text-center">
       <h3    className="mb-3 block text-2xl font-bold 	text-cyan-400 hover:text-purple-300 uppercase">
